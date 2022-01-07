82a9db938c4288e290f01b15e9781a9fa20a28e0
kashish-vaish <96825288+kashish-vaish@users.noreply.github.com>
2022-01-07 17:31:30 +0530

    Commit FetchName and FirstProject java files

--- src/FetchName.java
+++ src/FetchName.java
@@ -1,11 +1,20 @@
 public class FetchName {
     public String name;
+    public String lastName;
     public void setName(String name)
     {
         this.name = name;
     }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
     public String getName()
     {
         return name;
+    }
+    public String getLastName()
+    {
+        return lastName;
     }
 }
